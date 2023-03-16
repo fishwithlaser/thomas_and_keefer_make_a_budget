2e973157c79ffdd42982654c41f90bd56281c45d
fishwithlaser <tomaszkosciuch@gmail.com>
2023-03-15 20:45:04 -0400

    uploading transaction data

---
+++ common/stupid.py
@@ -0,0 +1,17 @@
+import random
+
+def lstr(str_:str, length=10) -> str:
+    str_ = str(str_)
+    return str_[0:length] + " " * (length - len(str_))
+
+def fun(letter:str) -> str:
+    return f'\33[{random.randrange(0,255)};{random.randrange(0,255)};{random.randrange(0,255)}m{letter}\033[0;0m'
+
+def fstr(str_:str):
+    """a more fun type of fstring"""
+    return_str = ''
+    for l in str_:
+        return_str += fun(l)
+    return return_str
+    
+STUPID = "\033[48;5;236m\033[38;5;0mSu\033[23;44;44mcc\033[38;5;208mess\033[0;0m"
---
+++ database/transactions.py
@@ -0,0 +1,25 @@
+from datetime import date
+from decimal import Decimal
+from sqlalchemy.types import String, Date, DECIMAL
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+
+from .frequencies import Recursion 
+from .common import Base
+from common.stupid import lstr
+
+class Transactions(Base):
+
+    def __repr__(self) -> str:
+        return f'{lstr(self.Name, 10)} - {lstr(self.Amount)} - {lstr(self.Category)}'
+
+    __tablename__ = 'Transactions'
+    Id: Mapped[int] = mapped_column(primary_key=True)
+    TransactionId: Mapped[str] = mapped_column(String)
+    Name: Mapped[str] = mapped_column(String)
+    Amount: Mapped[Decimal] = mapped_column(DECIMAL)
+    Recursion: Mapped[str] = mapped_column(ForeignKey(Recursion.Name), nullable=True, default=None)
+    Description: Mapped[str] = mapped_column(String, nullable=True)
+    TransactionDate: Mapped[date] = mapped_column(Date)
+    Category: Mapped[str] = mapped_column(String)
+
